4447b498654c6ca7a1c343ed36987b16c9edb9e6
Leonardo Nicolas <leonicolas@gmail.com>
2020-07-24 07:26:11 -0700

    Add audio player and test audio files

--- lib/bricks_build.dart
+++ lib/bricks_build.dart
@@ -1,6 +1,10 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 class BricksBuild extends StatelessWidget {
+
+  AudioCache player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -11,11 +15,18 @@ class BricksBuild extends StatelessWidget {
         crossAxisCount: 4,
         children: new List<Widget>.generate(16, (index) {
           return new GridTile(
-            child: new Card(
-              color: Colors.red.shade200,
-              child: new Center(
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: FlatButton(
+                color: Colors.red.shade300,
                 child: new Text('$index'),
-              ),
+                onPressed: () async {
+                  String sound = index % 2 == 0
+                      ? 'bell_ring.mp3'
+                      : 'cow.mp3';
+                  await player.play(sound);
+                },
+              )
             )
           );
         }),
